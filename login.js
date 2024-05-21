fa06fe4b28385ba0baf97ea9be0903c3e6149a6a
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-21 17:25:09 +0500

    Login Page

--- login.js
+++ login.js
@@ -2,10 +2,12 @@ const username = document.querySelector(`.Username`)
 const password = document.querySelector(`.password`)
 const btn = document.querySelector(`.submit`)
 const form = document.querySelector(`form`)
-
+const h1 = document.querySelector(`h1`)
 
 form.addEventListener(`submit` , function(event) {
     event.preventDefault();
-    console.log(username.value);
-    console.log(password.value);
+    console.log(`Username:` , username.value);
+    console.log(`Password` , password.value);
+
+    h1.innerHTML = `Welcome Back ${username.value}`
 })
